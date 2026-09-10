c0a6041809aab99ba11c3fa4382500f0c7b0fe03
Your Name <you@example.com>
2026-09-11 01:20:46 +0330

    feat():add enum for order status

---
+++ app/schemas/config.py
@@ -0,0 +1,9 @@
+from enum import Enum
+
+class OrderStatus(str, Enum):
+    PENDING = "pending"
+    PAID = "paid"
+    PROCESSING = "processing"
+    COMPLETED = "completed"
+    CANCELLED = "cancelled"
+
