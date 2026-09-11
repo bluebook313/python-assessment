7fe4d0401270d5b3242d0277b51c296ef54d1e47
Your Name <you@example.com>
2026-09-11 16:24:47 +0330

    feat(app\api\v1\product.py):add  api for adding product to db

--- app/api/v1/product.py
+++ app/api/v1/product.py
@@ -3,8 +3,8 @@ from fastapi import status, HTTPException
 from utils.api_response.response import CustoneResponse
 router = APIRouter()
 
-from services.task.tasks import GetProductsTasks, GetProductByIdTasks
-                                
+from services.task.tasks import GetProductsTasks, GetProductByIdTasks, AddProductTasks
+from schemas.schema import  ProductStructure                           
 
 
 async def handle_task(task_class, **kwargs):
@@ -12,12 +12,14 @@ async def handle_task(task_class, **kwargs):
         result =  await task_class.run(**kwargs)
         return CustoneResponse.response(
             status=status.HTTP_200_OK,
-            details=f"task successfully submited - {result}"
+            details=f"task successfully submited",
+            result = result
         )       
     except Exception as e:
         return CustoneResponse.response(
             status=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            details=str(e)
+            details=str(e),
+            result={}
         )
 
 @router.post("/products")
@@ -26,5 +28,9 @@ async def get_products():
 
 
 @router.get("/products/{product_id}")
-async def  get_order(product_id: int):
+async def  get_product(product_id: int):
     return await handle_task(task_class=GetProductByIdTasks, product_id=product_id)
+
+@router.post("/products/add_product/")
+async def  add_products(params:ProductStructure): 
+    return await handle_task(task_class=AddProductTasks, name=params.name, count=params.product_count, price=params.price)
--- app/services/product/product.py
+++ app/services/product/product.py
@@ -1,5 +1,38 @@
-class Product:
+from sqlalchemy import select
+from models import Product
+from sqlalchemy.orm import Session
+
+class ProductService:
+
+    def __init__(self, db: Session):
+        self.db = db
+
+    async def add_item(self, name: str, price: int, count:int):
+        product = Product(
+            name=name,
+            price=price,
+            count=count
+        )
+
+        self.db.add(product)
+        self.db.commit()
+
+        return product
+    
+    
     async def get_all_item(self):
-        return True
-    async def get_item(self, product_id):
-        return {"product_id":product_id}
+        product_list =  self.db.scalars(
+            select(Product)
+        ).all()
+        return {pr.id:{"Name":pr.name, "Price":pr.price, "Count":pr.count} for pr in product_list}
+            
+        
+
+    async def get_item(self, product_id: int):
+        product =  self.db.get(Product, product_id)
+        return {
+                    "Id":product.id, 
+                    "Name":product.name, 
+                    "Price":product.price, 
+                    "Count":product.count
+            } 
