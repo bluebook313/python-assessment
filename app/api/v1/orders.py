94124389e87796f258e38d97268ed7b637d0bea6
Your Name <you@example.com>
2026-09-11 03:49:30 +0330

    feat(*):handel exception and refactore usage

--- app/api/v1/orders.py
+++ app/api/v1/orders.py
@@ -1,26 +1,40 @@
 
 from fastapi import APIRouter
 from fastapi import status, HTTPException
+from utils.api_response.response import CustoneResponse
 router = APIRouter()
 
+from services.task.tasks import ( GetOrdersTasks, GetOrderByIdTasks, 
+                                  PayOrderByIdTasks, CancelOrderByIdTasks
+                                )
 
 
+async def handle_task(task_class, **kwargs):
+    try:
+        result =  await task_class.run(**kwargs)
+        return CustoneResponse.response(
+            status=status.HTTP_200_OK,
+            details=f"task successfully submited - {result}"
+        )       
+    except Exception as e:
+        return CustoneResponse.response(
+            status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            details=str(e)
+        )
 
 @router.post("/orders")
-def get_orders():
-    return {}
+async def get_orders():
+    return await handle_task(task_class=GetOrdersTasks)
 
 
-@router.get("/orders/{id}")
-def get_order(id: int):
-    return {}
+@router.get("/orders/{order_id}")
+async def  get_order(order_id: int):
+    return await handle_task(task_class=GetOrderByIdTasks, order_id=order_id)
 
-@router.post("/orders/{id}/pay")
-def get_order(id: int):
-    return {}
+@router.post("/orders/{order_id}/pay")
+async def  pay_order(order_id: int):
+    return await handle_task(task_class=PayOrderByIdTasks, order_id=order_id)
 
-@router.post("/orders/{id}/cancel")
-def get_order(id: int):
-    return {}
-
-
+@router.post("/orders/{order_id}/cancel")
+async def  cancel_order(order_id: int):
+    return await handle_task(task_class=CancelOrderByIdTasks, order_id=order_id)
--- app/api/v1/product.py
+++ app/api/v1/product.py
@@ -1,16 +1,30 @@
-
 from fastapi import APIRouter
 from fastapi import status, HTTPException
+from utils.api_response.response import CustoneResponse
 router = APIRouter()
 
+from services.task.tasks import GetProductsTasks, GetProductByIdTasks
+                                
 
 
+async def handle_task(task_class, **kwargs):
+    try:
+        result =  await task_class.run(**kwargs)
+        return CustoneResponse.response(
+            status=status.HTTP_200_OK,
+            details=f"task successfully submited - {result}"
+        )       
+    except Exception as e:
+        return CustoneResponse.response(
+            status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            details=str(e)
+        )
 
-@router.get("/products")
-def get_products():
-    return {}
+@router.post("/products")
+async def get_products():
+    return await handle_task(task_class=GetProductsTasks)
 
 
-@router.get("/products/{id}")
-def get_product(id: int):
-    return {}
+@router.get("/products/{product_id}")
+async def  get_order(product_id: int):
+    return await handle_task(task_class=GetProductByIdTasks, product_id=product_id)
